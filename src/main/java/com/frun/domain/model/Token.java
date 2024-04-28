3b430efa25842ec4088080285206045322198349
xogus0226 <qkdrhks123@gmail.com>
2024-04-28 16:41:06 +0900

    [Update] Token 엔티티 생성전략 변경

--- src/main/java/com/frun/domain/model/Token.java
+++ src/main/java/com/frun/domain/model/Token.java
@@ -2,11 +2,12 @@ package com.frun.domain.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "token")
 public class Token {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String token;
 }
