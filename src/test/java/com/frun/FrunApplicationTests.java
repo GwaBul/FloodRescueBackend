83e808617166dd8e8c84c418bdd9d26acca91c23
xogus0226 <qkdrhks123@gmail.com>
2024-05-13 15:35:53 +0900

    [Save] 금오천, 하천천 데이터 DB에 저장

--- src/test/java/com/frun/FrunApplicationTests.java
+++ src/test/java/com/frun/FrunApplicationTests.java
@@ -1,13 +1,54 @@
 package com.frun;
 
+import com.frun.domain.interfaces.CityRepository;
+import com.frun.domain.interfaces.RiverRepository;
+import com.frun.domain.model.City;
+import com.frun.domain.model.River;
+import com.frun.domain.service.CityService;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.MultiPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FrunApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(FrunApplicationTests.class);
+    @Autowired
+    private CityService cityService;
+    @Autowired
+    private RiverRepository riverRepository;
+    @Autowired
+    private CityRepository cityRepository;
 
     @Test
-    void contextLoads() {
+    void saveCity() {
+        cityService.createCity("구미");
     }
 
+    @Test
+    void saveRiverName() {
+        City city = cityRepository.findByName("부산");
+        River river = River.builder()
+                .name("온천천")
+                .city(city)
+                .build();
+        riverRepository.save(river);
+    }
+
+    @Test
+    void saveExitList() {
+        Coordinate[] exits = new Coordinate[] {
+
+        };
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+        MultiPoint points = geometryFactory.createMultiPointFromCoords(exits);
+        River foundRiver = riverRepository.findByName("온천천");
+        foundRiver.setExitList(points);
+        riverRepository.save(foundRiver);
+    }
 }
